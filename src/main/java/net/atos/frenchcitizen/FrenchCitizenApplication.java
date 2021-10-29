45f9ce1b1441662b3aa1cf5d4fb6f9f53e62edb1
CLOP, THOMAS <thomas.clop@atos.net>
2021-10-29 11:34:45 +0200

    init repo

---
+++ src/main/java/net/atos/frenchcitizen/FrenchCitizenApplication.java
@@ -0,0 +1,13 @@
+package net.atos.frenchcitizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchCitizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenchCitizenApplication.class, args);
+	}
+
+}
